1bde3592d3fa61d029a90dfadf6b02dddba38236
mcholbrook <mirandacholbrook@gmail.com>
2021-03-18 15:21:44 -0400

    Add models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+class Profile(models.Model):
+  bio = models.TextField(max_length=200, blank=True)
+  photo = models.CharField(max_length=200, blank=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Profile for user: {self.user.first_name} at user_id {self.user_id}."
+
+class Restaurant(models.Model):
+  name = models.TextField(max_length=100)
+  location = models.TextField(max_length=300, blank=True)
+  phone = models.TextField(max_length=15, blank=True)
+  website = models.TextField(max_length=200, blank=True)
+  creator = models.TextField(max_length=100, blank=True)
+  
+  def __str__(self):
+    return f"This is restaurant: {self.name}"
+
+class Note(models.Model):
+  date = models.DateField(default=datetime.now)
+  content = models.TextField(max_length=500)
+  restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.content} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
--- restaurants/settings.py
+++ restaurants/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+environ.Env()
+environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jn7_-&e_!30gk_dqip=$)%3@#fn^p#p%$@_p)0kxxkhqw^fk+-'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
